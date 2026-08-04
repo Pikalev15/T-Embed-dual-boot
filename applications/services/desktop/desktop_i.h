808e72769098118624bf294d8afd21204615af05
Levi <pehlevi224@gmail.com>
2026-08-04 12:29:11 +0800

    fix: keep selector inside existing Desktop dialog infrastructure

--- applications/services/desktop/desktop_i.h
+++ applications/services/desktop/desktop_i.h
@@ -8,7 +8,6 @@
 #include "views/desktop_view_pin_input.h"
 #include "views/desktop_view_locked.h"
 #include "views/desktop_view_main.h"
-#include "views/desktop_view_boot_selector.h"
 #include "views/desktop_view_lock_menu.h"
 #include "views/desktop_view_usb_storage.h"
 #include "views/desktop_view_mesh_clients.h"
@@ -37,7 +36,6 @@
 
 typedef enum {
     DesktopViewIdMain,
-    DesktopViewIdBootSelector,
     DesktopViewIdLockMenu,
     DesktopViewIdUsbStorage,
     DesktopViewIdMeshClients,
@@ -70,7 +68,6 @@ struct Desktop {
 
     Popup* popup;
     DialogEx* mesh_pair_dialog;
-    DesktopBootSelectorView* boot_selector_view;
     DesktopLockMenuView* lock_menu;
     DesktopUsbStorageView* usb_storage_view;
     DesktopMeshClientsView* mesh_clients_view;
@@ -145,7 +142,7 @@ void desktop_lock(Desktop* desktop);
 void desktop_unlock(Desktop* desktop);
 void desktop_set_dummy_mode_state(Desktop* desktop, bool enabled);
 void desktop_set_stealth_mode_state(Desktop* desktop, bool enabled);
-bool desktop_boot_selector_available(void);
+bool desktop_boot_selector_should_show(void);
 
 /* Mesh-Callback (impl in desktop.c): packt das Event in desktop->mesh_pending
  * und feuert DesktopMeshEventClient{PairRequest,Disconnect} via
